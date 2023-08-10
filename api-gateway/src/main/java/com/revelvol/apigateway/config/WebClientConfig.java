db64088383c1507e5de3a051e552bc7cb3b50ac3
Revelvol <julius.sutandi@gmail.com>
2023-08-10 14:35:11 +0700

    added prometheus and grafana monitoring, fixed bug for webclient loader failed in apigateway and maitnenance ticket

--- api-gateway/src/main/java/com/revelvol/apigateway/config/WebClientConfig.java
+++ api-gateway/src/main/java/com/revelvol/apigateway/config/WebClientConfig.java
@@ -1,6 +1,10 @@
 package com.revelvol.apigateway.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClientsProperties;
+import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -8,7 +12,19 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
-
+    @Bean
+    @ConditionalOnMissingBean
+    public LoadBalancerClientFactory loadBalancerClientFactory(LoadBalancerClientsProperties properties) {
+        return new LoadBalancerClientFactory(properties) {
+            @Override public AnnotationConfigApplicationContext createContext(String name) {
+                ClassLoader originalClassLoader = Thread.currentThread().getContextClassLoader();
+                Thread.currentThread().setContextClassLoader(this.getClass().getClassLoader());
+                AnnotationConfigApplicationContext context = (AnnotationConfigApplicationContext) super.createContext(name);
+                Thread.currentThread().setContextClassLoader(originalClassLoader);
+                return context;
+            }
+        };
+    }
 
     @Bean // @LoadBalanced because there might be multiple service
     @LoadBalanced
